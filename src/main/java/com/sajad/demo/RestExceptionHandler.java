8f53d896fbd27909e0bffbd460c20cfb895677ed
sajad Hayatlou <sajjad.htlo@gmail.com>
2021-03-19 15:22:53 +0330

    No need to use mysql, an h2 used for simplicity.

--- src/main/java/com/sajad/demo/RestExceptionHandler.java
+++ src/main/java/com/sajad/demo/RestExceptionHandler.java
@@ -7,15 +7,11 @@ import com.sajad.demo.exception.VoteNotAllowedException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static com.sajad.demo.utility.Constants.ErrorMessages.COMMENT_NOT_ALLOWED_MSG;
-import static com.sajad.demo.utility.Constants.ErrorMessages.VOTE_NOT_ALLOWED_MSG;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static com.sajad.demo.helper.Constants.ErrorMessages.*;
 
 /**
  * Rest exception handler to catch the exceptions and provide a more meaningful message
@@ -29,24 +25,24 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CommentNotAllowedException.class)
     protected ResponseEntity<Object> handleCommentNotAllowed() {
-        ErrorResponseDto responseDto = new ErrorResponseDto(COMMENT_NOT_ALLOWED_MSG + "xx");
+        ErrorResponseDto responseDto = new ErrorResponseDto(COMMENT_NOT_ALLOWED_MSG);
 
         return ResponseEntity.badRequest().body(responseDto);
     }
 
     @ExceptionHandler(VoteNotAllowedException.class)
     protected ResponseEntity<Object> handleVoteNotAllowed() {
-        ErrorResponseDto responseDto = new ErrorResponseDto(VOTE_NOT_ALLOWED_MSG);
+        ErrorResponseDto responseDto = new ErrorResponseDto(RATE_NOT_ALLOWED_MSG);
 
         return ResponseEntity.badRequest().body(responseDto);
     }
-
-    // We'll need to catch other type of exceptions as well, e.g., 404, validation and binding errors
 
     @ExceptionHandler(ResourceNotFoundException.class)
     protected ResponseEntity<Object> handleNotFound() {
-        ErrorResponseDto responseDto = new ErrorResponseDto("Not found");
+        ErrorResponseDto responseDto = new ErrorResponseDto(ENTITY_NOT_FOUND_MSG);
 
         return ResponseEntity.badRequest().body(responseDto);
     }
+
+    // We'll need to catch other type of exceptions as well, e.g., validation and binding errors
 }
